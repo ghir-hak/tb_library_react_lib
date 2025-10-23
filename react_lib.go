effc031491aad413d3b5b27605be774c4dbece80
Zaoui Amine <dev.zaoui@gmail.com>
2025-10-23 20:12:25 +0100

    -

--- react_lib.go
+++ react_lib.go
@@ -1,10 +1,52 @@
-package lib 
+package lib
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+
 	"github.com/taubyte/go-sdk/event"
+	http "github.com/taubyte/go-sdk/http/event"
+	pubsub "github.com/taubyte/go-sdk/pubsub/node"
 )
 
-//export function
-func function(e event.Event) uint32 {
-  return 0
+func fail(h http.Event, err error, code int) uint32 {
+	h.Write([]byte(err.Error()))
+	h.Return(code)
+	return 1
+}
+
+//export getsocketurl
+func getsocketurl(e event.Event) uint32 {
+	h, err := e.HTTP()
+	if err != nil {
+		return 1
+	}
+
+	// get room from query
+	room, err := h.Query().Get("room")
+	if err != nil {
+		return fail(h, err, 500)
+	}
+
+	// hash the room to create a channel name
+	hash := md5.New()
+	hash.Write([]byte(room))
+	roomHash := hex.EncodeToString(hash.Sum(nil))
+
+	// create/open a channel with the hash
+	channel, err := pubsub.Channel("chat-" + roomHash)
+	if err != nil {
+		return fail(h, err, 500)
+	}
+
+	// get the websocket url
+	url, err := channel.WebSocket().Url()
+	if err != nil {
+		return fail(h, err, 500)
+	}
+
+	// write the url to the response
+	h.Write([]byte(url.Path))
+
+	return 0
 }
